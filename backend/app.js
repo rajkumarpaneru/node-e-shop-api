7eef5d056f0465f872aa7b96011c90cd55a221fc
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2022-09-16 21:25:31 +0545

    add .env.example, install dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const app = express();
 
+require('dotenv/config');
+
+const api = process.env.API_URL;
+
 
 app.get('/', (req, res)=> {
     res.send('hello api');
 });
 app.listen(3000, ()=>{
-    console.log('Server is running.');
+    console.log(api);
 })
