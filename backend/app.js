37cd1896e1b42bf2a07f426e4268c721ad765e3f
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2022-09-16 21:57:05 +0545

    products get and post demo

--- backend/app.js
+++ backend/app.js
@@ -1,14 +1,32 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+
+
+//middleware
+app.use(bodyParser.json());
 
 require('dotenv/config');
 
 const api = process.env.API_URL;
 
 
-app.get('/', (req, res)=> {
-    res.send('hello api');
+app.get('/products', (req, res)=> {
+    
+    const product = {
+        id: 1,
+        name: 't-shirt',
+        image: 'some_url',
+    };
+    res.send(product);
 });
+
+app.post('/products', (req, res)=> {
+    const newProduct = req.body;
+
+    res.send(newProduct);
+});
+
 app.listen(3000, ()=>{
     console.log(api);
 })
