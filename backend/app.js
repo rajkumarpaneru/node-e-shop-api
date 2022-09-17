983e43508a7fad604e9af0642f96546f7d99185e
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2022-09-17 20:58:08 +0545

    use morgan to log request

--- backend/app.js
+++ backend/app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
 
 //middleware
 app.use(bodyParser.json());
+
+app.use(morgan('tiny'));
 
 require('dotenv/config');
 
