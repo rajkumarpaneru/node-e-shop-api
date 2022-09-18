cf6572bd90c8b9ed94fce1070ece680b16e7e047
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2022-09-18 09:36:55 +0545

    npm install mongoose

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
 
 
 //middleware
