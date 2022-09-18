2872a8f725fc34f6f7a05f5282465398e3eeac11
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2022-09-18 14:26:55 +0545

    separate routes and model files for products

--- backend/app.js
+++ backend/app.js
@@ -3,62 +3,21 @@ const app = express();
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-
+require('dotenv/config');
 
 //middleware
 app.use(bodyParser.json());
-
 app.use(morgan('tiny'));
 
-require('dotenv/config');
+
+const productsRoutes = require('./routes/products');
 
 const api = process.env.API_URL;
 
-
-//make schema
-const productSchema = mongoose.Schema({
-    name: String,
-    image: String,
-    countInStock: Number
-});
+app.use(`/products`, productsRoutes);
 
 
-//make model
-const Product = mongoose.model('Product', productSchema);
-
-
-app.get('/products', async (req, res)=> {
-    
-    const products = await Product.find();
-
-    if(!products){
-        res.status(500).json({success: false});
-    }
-    res.status(200).json(products);
-});
-
-app.post('/products', (req, res)=> {
-    // const newProduct = req.body;
-
-    const product = new Product({
-        name: req.body.name,
-        image: req.body.image,
-        countInStock: req.body.countInStock,
-    });
-
-    product.save()
-    .then((createdProduct)=>{
-        res.status(201).json(createdProduct);
-    })
-    .catch((err)=>{
-        res.status(500).json({
-            error: err,
-            success: false,
-        });
-    });
-
-});
-
+//Database
 mongoose.connect(process.env.CONNECTION_STRING,
     {
         useNewUrlParser: true,
