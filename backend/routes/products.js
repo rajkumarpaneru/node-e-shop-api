3bc1722de0a3dfe33db6e3f6a4d554b774043fbe
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2022-09-20 21:04:54 +0545

    update category endpoint

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -51,4 +51,36 @@ router.post(`/`, async(req, res) =>{
     res.send(cratedProduct);
 });
 
+router.put(`/:id`, async (req, res) => {
+
+    const category = await Category.findById(req.body.category);
+
+    if(!category){
+        return res.status(400).send('Invalid category');
+    }
+
+    const product = await Product.findByIdAndUpdate(
+        req.params.id,
+        {
+            name: req.body.name,
+            description: req.body.description,
+            richDescription: req.body.richDescription, 
+            image: req.body.image,
+            brand: req.body.brand,
+            price: req.body.price,
+            category: req.body.category,
+            countInStock: req.body.countInStock,
+            rating: req.body.rating,
+            numReviews: req.body.numReviews,
+            isFeatured: req.body.isFeatured,
+        },
+        {new: true}
+    )
+
+    if(!product)
+        res.status(500).send('Error while updating product');
+    
+        res.status(201).json(product);
+});
+
 module.exports =router;
