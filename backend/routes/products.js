60c2674b18970644b06a4dffcf09ccf59a1877f1
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2022-09-20 21:01:39 +0545

    populate category when getting product by id

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -13,7 +13,7 @@ router.get(`/`, async (req, res) =>{
 })
 
 router.get(`/:id`, async (req, res) =>{
-    const product = await Product.findById(req.params.id);
+    const product = await Product.findById(req.params.id).populate('category');
 
     if(!product) {
         res.status(500).json({success: false})
