2223389abeb0563384feb4944e0d1a06fa68021e
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2022-09-25 22:20:12 +0545

    exclude routes from auth middleware

--- backend/helpers/jwt.js
+++ backend/helpers/jwt.js
@@ -4,7 +4,14 @@ function authJwt(){
     return expressJwt({
         secret: 'secret',
         algorithms: ['HS256']
-    });
+    }).unless({
+        path: [
+             {url: /\/api\/v1\/products(.*)/, methods: ['GET','OPTIONS']},
+             {url: /\/api\/v1\/categories(.*)/, methods: ['GET','OPTIONS']},
+             '/api/v1/users/register',
+             '/api/v1/users/login',
+        ]
+    })
 }
 
 module.exports = authJwt;
