51d4b1b4f0b021db120a92bbb53f89cef1275d30
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2022-10-11 20:12:02 +0545

    populate user name in orders

--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -4,7 +4,7 @@ const { OrderItem } = require('../models/order-item');
 const router = express.Router();
 
 router.get(`/`, async (req, res) => {
-    const orderList =await Order.find();
+    const orderList =await Order.find().populate('user');
 
     if(!orderList){
         return res.status(500).json({success: false});
