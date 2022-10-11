bea2f2906a8644518d2283f3abc374477bbc1524
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2022-10-11 20:33:45 +0545

    endpoints to update order status and delete order

--- backend/models/order.js
+++ backend/models/order.js
@@ -43,3 +43,13 @@ const orderSchema = mongoose.Schema({
         default: Date.now,
     }
 })
+
+orderSchema.virtual('id').get(function(){
+    return this._id.toHexString();
+});
+
+orderSchema.set('toJSON', {
+    virtuals: true
+});
+
+exports.Order = mongoose.model('order', orderSchema);
--- backend/routes/orders.js
+++ backend/routes/orders.js
@@ -15,7 +15,7 @@ router.get(`/`, async (req, res) => {
 router.get(`/:id`, async(req, res) => {
     const order = await Order.findById(req.params.id)
     .populate('user', 'name')
-    .populate({path: 'orderItems', populate: 'product'});
+    .populate({path: 'orderItems', populate: {path: 'product', populate: 'category'}});
 
     if(!order){
         res.status(500).json({success: false});
@@ -58,6 +58,36 @@ router.post(`/`, async (req, res) =>{
         res.status(400).send('Error while creating order.');
     
     res.status(201).json(order);
+});
+
+router.put(`/:id`, async (req, res) => {
+    const order = await Order.findByIdAndUpdate(
+        req.params.id,
+        {
+            status: req.body.status 
+        },
+        {new: true}
+    )
+
+    if(!order)
+        res.status(500).send('Error while updating order status');
+    
+    res.status(201).json(order);
+});
+
+router.delete(`/:id`, async (req, res) => {
+    //TODO: Delete order items after order is deleted
+    Order.findByIdAndRemove(req.params.id)
+    .then(order => {
+        if(order){
+            return res.status(200).json({success: true, message: 'Order is deleted.'});
+        } else {
+            return res.status(404).json({success: false, message: 'Order not found'})
+        }
+    })
+    .catch(err => {
+        return res.status(400).json({success:false, error: err});
+    });
 });
 
 module.exports = router;
