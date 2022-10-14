e197ea64ae673180154beeab4fceaa3d87f206f9
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2022-10-14 08:32:39 +0545

    upload image for product

--- backend/routes/products.js
+++ backend/routes/products.js
@@ -6,13 +6,26 @@ const mongoose = require('mongoose');
 const multer = require('multer');
 
 
+const FILE_TYPE_MAP = {
+    'image/png': 'png',
+    'image/jpeg': 'jpeg',
+    'image/jpg': 'jpg',
+}
+
 var storage = multer.diskStorage({
     destination: function (req, file, cb){
-        cb(null, '/public/uploads')
+        const isValid = FILE_TYPE_MAP[file.mimetype];
+        let uploadError = new Error('invalid image type');
+
+        if(isValid){
+            uploadError = null;
+        }
+        cb(uploadError, '/public/uploads')
     },
     filename: function(req, file, cb){
         const fileName = file.originalname.split(' ').join('-');
-        cb(null, fileName + '-' + Date.now())
+        const extension = FILE_TYPE_MAP[file.mimetype];
+        cb(null, `${fileName}-${Date.now()}.${$extension}`)
     } 
 });
 
@@ -52,6 +65,11 @@ router.post(`/`, uploadOptions.single('image'), async(req, res) =>{
     if(!category){
         return res.status(400).send('Invalid category');
     }
+
+    const file = req.file;
+
+    if(!file)
+        return res.status(400).send('No image in the request.')
 
     const fileName = req.file.filename;
     const basePath = `${req.protocol}://${req.get('host')}/public/uploads/`;
