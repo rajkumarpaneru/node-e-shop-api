57d1f230a5b76298b7db7467b8bebddd0624bd61
Raj Kumar Paneru <raajkumarpaneru@gmail.com>
2022-10-15 21:59:35 +0545

    fix: add static path

--- backend/app.js
+++ backend/app.js
@@ -15,7 +15,7 @@ app.options('*', cors);
 app.use(bodyParser.json());
 app.use(morgan('tiny'));
 app.use(authJwt);
-app.use('public/uploads', express.static(__dirname) + 'public/uploads');
+app.use('public/uploads', express.static(__dirname + 'public/uploads'));
 app.use(errorHandler);
 app.use((err, req, res, next) => {
     if(err){
